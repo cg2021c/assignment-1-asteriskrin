1f4f7acbac61f66176dce4e3011d9621ba5e8d04
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2021-12-10 10:55:45 +0700

    Challenge #6
    The light source position isn't updated yet

--- main3D.js
+++ main3D.js
@@ -14,7 +14,7 @@ function main() {
     camera.setPosition(0.3, 0.5, 2.1);
 
     var cube = new LightObject(gl);
-    cube.setPosition([0.0, 1.0, -5.0]);
+    cube.setPosition([0.0, .0, -3.0]);
     cube.setScale([0.075, 0.075, 0.075]);
     cube.setRotation([45.0/180.0*Math.PI, 45.0/180.0*Math.PI, 45.0/180.0*Math.PI]);
     cube.setIntensity(0.200+0.063); // The last 3 digits of my NRP is 063, then the intensity is set to 0.200 + 0.063 = 0.263
@@ -73,6 +73,72 @@ function main() {
     document.getElementById("cameraX").innerHTML = camera.cameraX;
     document.getElementById("cameraY").innerHTML = camera.cameraY;
     document.getElementById("cameraZ").innerHTML = camera.cameraZ;
+
+    // Interactive orbital rotation with mouse using quaternion concept
+    var lastPointOnTrackBall, currentPointOnTrackBall;
+    var lastQuat = glMatrix.quat.create();
+    function computeCurrentQuat() {
+        // Secara berkala hitung quaternion rotasi setiap ada perubahan posisi titik pointer mouse
+        var axisFromCrossProduct = glMatrix.vec3.cross(glMatrix.vec3.create(), lastPointOnTrackBall, currentPointOnTrackBall);
+        var angleFromDotProduct = Math.acos(glMatrix.vec3.dot(lastPointOnTrackBall, currentPointOnTrackBall));
+        var rotationQuat = glMatrix.quat.setAxisAngle(glMatrix.quat.create(), axisFromCrossProduct, angleFromDotProduct);
+        glMatrix.quat.normalize(rotationQuat, rotationQuat);
+        return glMatrix.quat.multiply(glMatrix.quat.create(), rotationQuat, lastQuat);
+    }
+    // Memproyeksikan pointer mouse agar jatuh ke permukaan ke virtual trackball
+    function getProjectionPointOnSurface(point) {
+        var radius = canvas.width / 2;  // Jari-jari virtual trackball kita tentukan sebesar 1/2 lebar kanvas
+        var center = glMatrix.vec3.fromValues(canvas.width / 2, canvas.height / 2, 0);  // Titik tengah virtual trackball
+        var pointVector = glMatrix.vec3.subtract(glMatrix.vec3.create(), point, center);
+        pointVector[1] = pointVector[1] * (-1); // Flip nilai y, karena koordinat piksel makin ke bawah makin besar
+        var radius2 = radius * radius;
+        var length2 = pointVector[0] * pointVector[0] + pointVector[1] * pointVector[1];
+        if (length2 <= radius2) pointVector[2] = Math.sqrt(radius2 - length2); // Dapatkan nilai z melalui rumus Pytagoras
+        else {  // Atur nilai z sebagai 0, lalu x dan y sebagai paduan Pytagoras yang membentuk sisi miring sepanjang radius
+            pointVector[0] *= radius / Math.sqrt(length2);
+            pointVector[1] *= radius / Math.sqrt(length2);
+            pointVector[2] = 0;
+        }
+        return glMatrix.vec3.normalize(glMatrix.vec3.create(), pointVector);
+    }
+    
+    var rotationMatrix = glMatrix.mat4.create();
+    var dragging;
+    function onMouseDown(event) {
+        var x = event.clientX;
+        var y = event.clientY;
+        var rect = event.target.getBoundingClientRect();
+        // When the mouse pointer is inside the frame
+        if (
+            rect.left <= x &&
+            rect.right >= x &&
+            rect.top <= y &&
+            rect.bottom >= y
+        ) {
+            dragging = true;
+        }
+        lastPointOnTrackBall = getProjectionPointOnSurface(glMatrix.vec3.fromValues(x, y, 0));
+        currentPointOnTrackBall = lastPointOnTrackBall;
+    }
+    function onMouseUp(event) {
+        dragging = false;
+        if (currentPointOnTrackBall != lastPointOnTrackBall) {
+            lastQuat = computeCurrentQuat();
+        }
+    }
+    function onMouseMove(event) {
+        if (dragging) {
+            var x = event.clientX;
+            var y = event.clientY;
+            currentPointOnTrackBall = getProjectionPointOnSurface(glMatrix.vec3.fromValues(x, y, 0));
+            glMatrix.mat4.fromQuat(rotationMatrix, computeCurrentQuat());
+            console.log(rotationMatrix);
+            cube.rotationMatrix = rotationMatrix;
+        }
+    }
+    document.addEventListener('mousedown', onMouseDown);
+    document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('mousemove', onMouseMove);
 }
 
 window.onload = () => {
